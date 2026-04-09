85ddf80e093305f73c3bfda59afe0dfc65b53126
Laboratorio <laboratorio@udla-ec.int>
2026-04-09 17:45:18 -0500

    Subido

---
+++ src/Clinica.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.concurrent.ExecutionException;
+
+public class Clinica {
+
+    private PriorityQueue<Paciente> colaPrioridad;
+
+    public Clinica(){
+        colaPrioridad=new PriorityQueue<>();
+    }
+
+    public void encolar(Paciente paciente){
+        colaPrioridad.add(paciente);
+    }
+
+    public Paciente desencolar() throws Exception {
+        if (colaPrioridad.isEmpty())
+            throw new Exception("No hay elementos");
+        return colaPrioridad.poll();
+    }
+
+    public ArrayList<Paciente> listar (){
+
+        return new ArrayList<>(colaPrioridad);
+
+    }
+}
